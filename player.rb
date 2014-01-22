510c0e33ce223c202f79c472d2ad977359296430
Ryan Stauffer <rstauffe@mymail.mines.edu>
2014-01-22 00:26:07 -0700

    level 7 complete

--- player.rb
+++ player.rb
@@ -1,11 +1,18 @@
 class Player
+	#measure if warrior has hit back wall yet
 	@@been_back = 0
 	
   def play_turn(warrior)
+		#read if under attack
 		is_under_attack(warrior)
-	
-			#initial movement is toward back wall
-		if (warrior.feel(:backward).empty? and @@been_back == 0)
+		
+		#check if facing "backward"
+		if warrior.feel.wall?
+			warrior.pivot!(:backward)
+			@@been_back = 1
+			
+			#initial movement is toward rear wall
+		elsif (warrior.feel(:backward).empty? and @@been_back == 0)
 			warrior.walk!(:backward)
 			
 			#captive checks
@@ -13,22 +20,26 @@ class Player
 			warrior.rescue!
 		elsif warrior.feel(:backward).captive?
 			warrior.rescue!(:backward)
-			#check if at back wall
-		elsif warrior.feel(:backward).wall?
-			@@been_back = 1
-			warrior.walk!
+			
+			#retreat if sufficiently wounded and not in melee range
 		elsif warrior.health < 10 and @incombat and warrior.feel.empty?
 			warrior.walk!(:backward)
 			
 			#attack if enemy in front
-		elsif (!warrior.feel.empty?)
+		elsif (!warrior.feel.empty? and !warrior.feel.wall?)
 			warrior.attack!
-		elsif !warrior.feel(:backward).empty?
+		elsif !warrior.feel(:backward).empty? and !warrior.feel(:backward).wall?
 			warrior.attack!(:backward)
 			
 			#heal if below threshold
-		elsif warrior.health < 16 and warrior.health >= @health
+		elsif warrior.health < 16 and !@incombat
 			warrior.rest!
+			
+			#check if at rear wall
+		elsif warrior.feel(:backward).wall?
+			@@been_back = 1
+			warrior.walk!
+			
 			#walk by default
 		else
 			warrior.walk!
